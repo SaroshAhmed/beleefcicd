8927eff8634686323a1d26235ff72ab09c5d93a3
sufiyanirfan3 <sufiyanirfan1613@gmail.com>
2024-09-21 00:28:44 +0500

    update

--- models/Booking.js
+++ models/Booking.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const vendorSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: false},
   mobile: { type: String, required: true },
 });
 
