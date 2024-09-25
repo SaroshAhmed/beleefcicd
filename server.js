8d64e30a5b6802328006dc71a54caabd8b2f1218
sufiyanirfan3 <sufiyanirfan1613@gmail.com>
2024-09-25 14:16:02 +0500

    update

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.get("/", (req, res) => {
 
 databaseConnect();
 bookingReminder();
-startPropertyUpdaterCron()
+// startPropertyUpdaterCron()
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
