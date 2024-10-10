73b976abb3d061e58d5ad8d9ba430fb0f46cb2d8
sufiyanirfan3 <sufiyanirfan1613@gmail.com>
2024-10-10 15:33:26 +0500

    update

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get("/", (req, res) => {
               <title>Welcome</title>
           </head>
           <body>
-              <h1>Welcome to Beleef Backend</h1>
+              <h1>Welcome to Beleef Backend APIs</h1>
               <p>This is backend page</p>
           </body>
       </html>
