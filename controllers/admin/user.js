1f40461f678f65f59ecef7a4eb94f25fc514d4d3
Muhammad Fahad <106611736+MFahad11@users.noreply.github.com>
2024-10-14 22:26:40 +0500

    update user api

--- controllers/admin/user.js
+++ controllers/admin/user.js
@@ -1,7 +1,7 @@
 const User = require("../../models/User");
 exports.getAllUsers = async (req, res) => {
     try {
-        const users = await User.find().select('name email mobile picture');
+        const users = await User.find().select('name email mobile picture company title');
         res.status(200).json({ success: true, data: users });
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
@@ -20,5 +20,28 @@ exports.getUser = async (req, res) => {
         res.status(200).json({ success: true, data: user });
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
+    }
+};
+exports.updateUser = async (req, res) => {
+    try{
+        const {
+            name,
+            mobile,
+            company,
+            title,
+            id
+        } = req.body;
+        const user = await User.findById(id);
+        if(!user){
+            return res.status(404).json({message: "User not found"});
+        }
+        user.name = name;
+        user.mobile = mobile;
+        user.company = company;
+        user.title = title;
+        await user.save();
+        res.status(200).json({success: true, data: user});
+    }catch(error){
+        res.status(500).json({success: false, message: error.message});
     }
 };
