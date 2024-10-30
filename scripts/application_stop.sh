6b20e5332dd5f87863b8e2b8f8a2ce6502324780
Sarosh Ahmed <100199208+SaroshAhmed@users.noreply.github.com>
2024-10-30 15:03:21 +0500

    Update application_stop.sh comment out stop script

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,16 @@
+# #!/bin/bash
+# #Stopping existing node servers
+# echo "Stopping any existing node servers"
+# pkill node
+
 #!/bin/bash
-#Stopping existing node servers
+# Stopping existing node servers
 echo "Stopping any existing node servers"
-pkill node
+
+# Attempt to kill the Node.js processes with sudo
+if sudo pkill -f node; then
+    echo "Successfully stopped existing node servers."
+else
+    echo "Failed to stop existing node servers. Please check permissions."
+    exit 1
+fi
